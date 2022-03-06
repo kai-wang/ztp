5b32d71a2d22a1543de46bf89ec98973834bb78f
Kai <simaow@outlook.com>
2022-03-06 16:40:35 +1100

    Introduce the logging

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@ use ztp::startup::run;
 use std::net::TcpListener;
 use ztp::configuration::get_configuration;
 use sqlx::PgPool;
+use env_logger::Env;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+  env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+  
   let configuration = get_configuration().expect("Failed to read configuration");
   let connection = PgPool::connect(&configuration.database.connection_string())
       .await
--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, App, HttpServer};
 use actix_web::dev::Server;
-use sqlx::postgres::PgColumn;
+use actix_web::web::Data;
+use actix_web::middleware::Logger;
 use std::net::TcpListener;
 use sqlx::PgPool;
 use crate::routes::{health_check, subscribe};
@@ -8,10 +9,11 @@ use crate::routes::{health_check, subscribe};
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
 
-  let db_pool = web::Data::new(db_pool);
+  let db_pool = Data::new(db_pool);
 
   let server = HttpServer::new(move || {    
     App::new()
+      .wrap(Logger::default())
       .route("/health_check", web::get().to(health_check))
       .route("/subscriptions", web::post().to(subscribe))
       // Register the connection as part of the application state
